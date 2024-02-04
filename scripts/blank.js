56ec2623d60967477700ef93ab4a5313b1ece21a
FreshPenguin112 <93781766+FreshPenguin112@users.noreply.github.com>
2024-02-03 20:42:35 -0600

    the update update

---
+++ scripts/blank.js
@@ -0,0 +1,6 @@
+(async () => {
+    var ext = vm.runtime.ext_jg3d;
+    var [renderer, Three, camera, scene] = [ext.renderer, ext.three, ext.camera, ext.scene];
+    //code goes here
+    
+})();
---
+++ scripts/raycastfrommouse.js
@@ -0,0 +1,36 @@
+(async () => {
+    function setupMouseListener() {
+        return new Promise((resolve, reject) => {
+            var ext = vm.runtime.ext_jg3d;
+            var [renderer, Three, camera, scene] = [ext.renderer, ext.three, ext.camera, ext.scene];
+
+            var rect = renderer.domElement.getBoundingClientRect();
+            var scaleX = renderer.domElement.width / rect.width;
+            var scaleY = renderer.domElement.height / rect.height;
+            var out = null;
+
+            function mousemoveHandler(event) {
+                var x = (event.clientX - rect.left) * scaleX;
+                var y = (event.clientY - rect.top) * scaleY;
+
+                var vector2 = new Three.Vector2(((x / renderer.domElement.width) * 2 - 1), (-(y / renderer.domElement.height) * 2 + 1));
+                //console.log(x);
+                const raycaster = new Three.Raycaster();
+                raycaster.setFromCamera(vector2, camera);
+                var intersects = raycaster.intersectObjects(scene.children, true);
+                out = intersects;
+                document.removeEventListener("mousemove", mousemoveHandler);
+                resolve(out);
+            }
+
+            document.addEventListener("mousemove", mousemoveHandler, { once: true });
+        });
+    }
+
+    try {
+        const result = await setupMouseListener();
+        return JSON.stringify(result)
+    } catch (error) {
+        return error.message
+    }
+})();
