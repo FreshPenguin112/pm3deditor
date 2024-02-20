21c0e2a83616dfe5584ffbe21ccfa57ec73c5e65
FreshPenguin112 <93781766+FreshPenguin112@users.noreply.github.com>
2024-02-20 09:33:19 -0600

    add stuff back cuz it was gone for some reason

---
+++ src/scripts/raycastfrommouse.js
@@ -0,0 +1,23 @@
+(() => {
+    var ext = vm.runtime.ext_jg3d;
+    var [mouse, renderer, Three, camera, scene] = [
+        vm.runtime.ioDevices.mouse,
+        ext.renderer,
+        ext.three,
+        ext.camera,
+        ext.scene,
+    ];
+    var rect = renderer.domElement.getBoundingClientRect();
+    var scaleX = renderer.domElement.width / rect.width;
+    var scaleY = renderer.domElement.height / rect.height;
+    var x = (mouse.getClientX() - rect.left) * scaleX;
+    var y = (mouse.getClientY() - rect.top) * scaleY;
+    var vector2 = new Three.Vector2(
+        (x / renderer.domElement.width) * 2 - 1,
+        -(y / renderer.domElement.height) * 2 + 1
+    );
+    const raycaster = new Three.Raycaster();
+    raycaster.setFromCamera(vector2, camera);
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    return JSON.stringify(intersects);
+})();
